901a1d9dd66f8b9458ba6733bd6f35c5b6288c7b
markwst <Wongsathorn_W@gmail.com>
2022-10-17 10:17:35 +0700

    deploy

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://52.23.234.62:8999/',
+  baseURL: 'http://54.83.89.173:8999/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
